fa8e7a8d3043fcd325b6e3499f747187194476f5
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2021-10-03 12:36:03 +0530

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -16,11 +16,11 @@ async function get(battery, phn_info) {
   return {
     msg:
       `*Whatsbot*\n\nThis chat is Powered By *Whatsbot*\n\n*Battery:* ${batttxt}\n*Device:* ${phn_info.device_manufacturer} ${phn_info.device_model}\n*WA Version:* ${phn_info.wa_version}\n*Whatsbot Version:* ${packageJson.version}\n*Pmpermit:* ${config.pmpermit_enabled}\n\n*Official Repository Url 👇*\n` +
-      "```https://github.com/TheWhatsBot/WhatsBot```",
+      "```https://github.com```",
     mimetype: "image/jpeg",
     data: Buffer.from(
       (
-        await axios.get("https://telegra.ph/file/ecbc27f276890bf2f65a2.jpg", {
+        await axios.get("https://telegra.ph/file/d190919562482ad00d469.jpg", {
           responseType: "arraybuffer",
         })
       ).data
